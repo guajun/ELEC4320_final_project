e2da56caa76ed0ed2c88a4451ac9a3d3e0cb719b
gua_jun <whgls323232@gmail.com>
2022-12-09 16:54:56 +0800

    create module, ready for test

---
+++ saw_tooth_wave.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+
+module saw_tooth_wave(
+    input reg clk,
+    input reg ena,
+    input reg amplitude,
+    input reg [15:0] prescaler,
+    output reg [15:0]data
+
+);
+
+reg [15:0] counter = 0;
+
+always @(posedge clk)
+begin
+    if(ena)
+    begin
+
+        if(counter == prescaler) 
+        begin
+            counter <= 0;
+
+            if(data == amplitude)data <= 0;
+
+            else data <= data + 1;
+
+        end
+        else counter <= counter + 1;  
+    end
+    else
+    begin
+        data <= 0;
+        counter <= 0;
+    end
+end
+
+endmodule
---
+++ triangular_wave.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+
+module triangular_wave(
+    input reg clk,
+    input reg ena,
+    input reg amplitude,
+    input reg [15:0] prescaler,
+    output reg [15:0]data
+
+);
+
+
+reg [15:0] counter = 0;
+reg inc = 0;
+
+always @(posedge clk)
+begin
+    if(ena)
+    begin
+        if(counter == prescaler) 
+        begin
+            counter <= 0;
+
+            if(data == amplitude)inc <= 0;
+
+            else if(data == 0)inc <= 0;
+
+            else data <= inc? data + 1: data - 1;
+        end
+        else counter <= counter + 1;
+    end
+    else
+    begin
+        data <= 0;
+        counter <= 0;
+    end
+end
+
+endmodule
+
+endmodule
