3bc772093068524840b767c0f3fbc41ffc2d3fd4
gua_jun <whgls323232@gmail.com>
2022-12-12 12:17:03 +0800

    optimize logic

--- src/function_generater.v
+++ src/function_generater.v
@@ -1,63 +1,88 @@
 `timescale 1ns / 1ps
 
 
-module function_generater();
-    
-    wire [15:0] data;
-    reg [7:0] control;
-    reg [15:0] prescaler;
-    reg [15:0] amplitude;
-    wire clk;
+module function_generater(output reg [15:0] data,
+                          input [7:0] control,
+                          input [15:0] prescaler,
+                          input [15:0] amplitude,
+                          input clk);
 
-    reg [7:0] mode;
+reg [7:0] mode;
+
+wire square_wave_data;
+wire [15:0] triangular_wave_data;
+wire [15:0] saw_tooth_wave_data;
+
+always @(posedge control[0])
+begin
+    case(control[3:1])
+        3'b000: mode <= 8'b0000_0001;
+        3'b001: mode <= 8'b0000_0010;
+        3'b010: mode <= 8'b0000_0100;
+        3'b011: mode <= 8'b0000_1000; // square wave
+        3'b100: mode <= 8'b0001_0000; // triangular wave
+        3'b101: mode <= 8'b0010_0000; // saw tooth wave
+        3'b110: mode <= 8'b0100_0000;
+        3'b111: mode <= 8'b1000_0000;
+    endcase
+end
+
+always @(negedge control[0])
+begin
+    mode <= 0;
+    data <= 0;
+end
+
+always @(posedge clk)
+
+case(mode)
+    8'b0000_0001:;
+    8'b0000_0010:;
+    8'b0000_0100:;
+    8'b0000_1000:data <= square_wave_data?16'hFFFF:16'h0000;
+    8'b0001_0000:data <= triangular_wave_data;
+    8'b0010_0000:data <= saw_tooth_wave_data;
+    8'b0100_0000:;
+    8'b1000_0000:;
     
-    always @(posedge clk)
-    begin
-        case(control[3:1])
-            3'b000: mode <= 8'b0000_0001;
-            3'b001: mode <= 8'b0000_0010;
-            3'b010: mode <= 8'b0000_0100;
-            3'b011: mode <= 8'b0000_1000;
-            3'b100: mode <= 8'b0001_0000;
-            3'b101: mode <= 8'b0010_0000;
-            3'b110: mode <= 8'b0100_0000;
-            3'b111: mode <= 8'b1000_0000;
-        endcase
-    end
+    default: data <= 0;
     
-    trigonometric_wave trigonometric_wave(
-    .clk(clk),
-    .ena(mode[2:0]),
-    .amplitude(amplitude),
-    .prescaler(prescaler),
-    .data(data)
-    );
-    
-    triangular_wave triangular_wave(
-    .clk(clk),
-    .ena(mode[4]),
-    .amplitude(amplitude),
-    .prescaler(prescaler),
-    .data(data)
-    );
-    
-    saw_tooth_wave saw_tooth_wave(
-    .clk(clk),
-    .ena(mode[5]),
-    .amplitude(amplitude),
-    .prescaler(prescaler),
-    .data(data)
-    );
-    
-    clk_wiz_0 instance_name(
-    // Clock out ports
-    .clk_out1(clk),     // output clk_out1
-    // Status and control signals
-    .reset(0), // input reset
-    .locked(0),       // output locked
-    // Clock in ports
-    .clk_in1(clk_in1) // input clk_in1
-    );
-    
-    // INST_TAG_END ------ End INSTANTIATION Template ---------
+endcase
+
+begin
+
+
+end
+
+//    trigonometric_wave trigonometric_wave(
+//    .clk(clk),
+//    .ena(mode[2:0]),
+//    .amplitude(amplitude),
+//    .prescaler(prescaler),
+//    .data(data)
+//    );
+
+square_wave square_wave(
+.clk(clk),
+.ena(mode[3]),
+.prescaler(prescaler),
+.data(square_wave_data)
+);
+
+triangular_wave triangular_wave(
+.clk(clk),
+.ena(mode[4]),
+.amplitude(amplitude),
+.prescaler(prescaler),
+.data(triangular_wave_data)
+);
+
+saw_tooth_wave saw_tooth_wave(
+.clk(clk),
+.ena(mode[5]),
+.amplitude(amplitude),
+.prescaler(prescaler),
+.data(saw_tooth_wave_data)
+);
+
 endmodule
